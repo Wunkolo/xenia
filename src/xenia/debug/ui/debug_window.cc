8acebda91cb2b0d92ed0a384b16a13acb1d7e359
Wunkolo <Wunkolo@gmail.com>
2024-04-27 17:26:42 -0700

    [ImGui] Stub ARM64 host debug text
    Marked as TODO for now

--- src/xenia/debug/ui/debug_window.cc
+++ src/xenia/debug/ui/debug_window.cc
@@ -970,6 +970,7 @@ void DebugWindow::DrawRegistersPane() {
     } break;
     case RegisterGroup::kHostGeneral: {
       ImGui::BeginChild("##host_general");
+#if XE_ARCH_AMD64
       for (int i = 0; i < 18; ++i) {
         auto reg = static_cast<X64Register>(i);
         ImGui::BeginGroup();
@@ -1007,6 +1008,9 @@ void DebugWindow::DrawRegistersPane() {
             i, thread_info->host_context.xmm_registers[i].f32);
         ImGui::EndGroup();
       }
+#elif XE_ARCH_ARM64
+      // TODO(wunkolo): print ARM64 registers
+#endif
       ImGui::EndChild();
     }
   }
